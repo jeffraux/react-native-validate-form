4f3b87b98a0b9d89c80fe5d3ecb4ebc43747e212
Jefferson Aux <auxcalibur@gmail.com>
2018-07-28 17:04:13 +0800

    Whitespace consistency udpate

--- src/finders/findValues.js
+++ src/finders/findValues.js
@@ -1,10 +1,10 @@
 const findValues = (requiredFields) => { // find all the values of required fields
-	return requiredFields.map(item => {
-		return { 
-			props: item.value.props, 
-			...item
-		}
-	});
+  return requiredFields.map(item => {
+    return { 
+      props: item.value.props, 
+      ...item
+    }
+  });
 }
 
 export default findValues;
